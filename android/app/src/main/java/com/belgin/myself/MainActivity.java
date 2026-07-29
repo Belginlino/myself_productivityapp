b82f800fb7a1b174f1965a181cf64b45226d5f5c
belginlino <belginlino485@gmail.com>
2026-07-29 15:27:15 +0530

    Configure GitHub Actions workflow for cloud APK build

---
+++ android/app/src/main/java/com/belgin/myself/MainActivity.java
@@ -0,0 +1,5 @@
+package com.belgin.myself;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.belgin.myself',
+  appName: 'myself-productivity-os',
+  webDir: 'dist'
+};
+
+export default config;
